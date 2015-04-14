7b56119f4a7aa4e97dd6222895d9a322dd1354cb
Eric Bellendir <e-bellendir@kellogg.northwestern.edu>
2015-04-13 20:23:57 -0500

    Had to copy paste. Copy to desktop does not work

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -4,19 +4,32 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-puts "Let's get the weather forecast for your address."
+puts "Let's get the weather forecast for your location."
 
-puts "What is the address you would like to know the weather for?"
-street_address = gets.chomp
-url_safe_street_address = URI.encode(street_address)
+puts "What is the latitude?"
+the_latitude = gets.chomp
 
-# Your code goes below.
+puts "What is the longitude?"
+the_longitude = gets.chomp
+
+# Your code goes below. Use the same approach as you did in
+#   address_to_coords.rb to read from a remote API and parse
+#   the results.
+
+url_of_data_we_want = "https://api.forecast.io/forecast/2fe02e8e6bfe071290046eeec157db50/"+ the_latitude + "," + the_longitude
+raw_data = open(url_of_data_we_want).read
+parsed_data = JSON.parse(raw_data)
+# puts parsed_data.to_s
+# ...
+the_temperature = parsed_data["currently"]["temperature"]
+the_hour_outlook = parsed_data["hourly"]["summary"]
+the_day_outlook = parsed_data["daily"]["summary"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{street_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
