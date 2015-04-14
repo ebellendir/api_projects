a5ff9f666cda5de6d1c7cd31b31875e12b9d06fe
Eric Bellendir <e-bellendir@kellogg.northwestern.edu>
2015-04-13 20:22:14 -0500

    Copy Paste from sublime.txt
    Could not sync so I had to copy paste

--- address_to_coords.rb
+++ address_to_coords.rb
@@ -16,19 +16,21 @@ street_address = gets.chomp
 url_safe_street_address = URI.encode(street_address)
 
 # Your code goes below. Hints:
-
-# url_of_data_we_want = "???"
-# raw_data = ???
-# parsed_data = ???
-
+# puts url_safe_street_address.to_s
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address="+ url_safe_street_address
+raw_data = open(url_of_data_we_want).read
+parsed_data = JSON.parse(raw_data)
+# puts parsed_data.to_s
 # ...
 
 # Let's store the latitude in a variable called 'the_latitude',
 #   and the longitude in a variable called 'the_longitude'.
 
-# the_latitude = ???
-# the_longitude = ???
+#puts parsed_data["results"][0]["geometry"]["location"]["lat"]
+
+the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
+puts "The latitude of #{street_address} is #{the_latitude} and the longitude is #{the_longitude}."
